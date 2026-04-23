86636e1145b40f4f0c40314aa42a947a0c7c2792
Deep <ddey26736@gmail.com>
2026-04-23 19:45:00 +0530

    Changes on the first class (bankAccount)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bank Management System (C++)
 
-This is a beginner-level project to learn C++ concepts like:
+This is a beginner level project to learn C++ concepts like:
 - Input/Output
 - Functions
 - OOP
--- main.cpp
+++ main.cpp
@@ -7,10 +7,24 @@ using namespace std;
 
 class bankAccount{
     private:
-    string bankName = "Bank of BMI";
+    string bankName = "Bank of BMI" + to_string(bankcode);
+    int bankcode;
     string accountHolderName;
     string accountNumber;
     string accountPassword;
+    string aadhaarNumber;
     string accountPhonenumber;
     double accountBalance;
+
+    public:
+    void savetoFile();
+    void loadFromFile();
+    void mainMenu();
+    void createAccount();
+    void loginBankAccount();
+    void depositMoney();
+    void withdrawMoney();
+    void checkBalance();
+    void accountDetails();
+    void allTransactions();
 };
